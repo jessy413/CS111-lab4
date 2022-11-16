fd4d79ff7d4f43b08c86dd898132b6984257ed17
jessy413 <jessy413@ucla.edu>
2022-11-15 23:59:10 -0800

    hello world entry done

--- ext2-create.c
+++ ext2-create.c
@@ -467,11 +467,11 @@ void write_hello_world_file_block(int fd)
 
 	bytes_remaining -= parent_entry.rec_len;
 
-	struct ext2_dir_entry hello_entry = {0};
-	dir_entry_set(hello_entry, HELLO_WORLD_INO, "hello-world");
-	dir_entry_write(hello_entry, fd);
+	// struct ext2_dir_entry hello_entry = {0};
+	// dir_entry_set(hello_entry, HELLO_WORLD_INO, "hello-world");
+	// dir_entry_write(hello_entry, fd);
 
-	bytes_remaining -= hello_entry.rec_len;
+	// bytes_remaining -= hello_entry.rec_len;
 
 	struct ext2_dir_entry fill_entry = {0};
 	fill_entry.rec_len = bytes_remaining;
